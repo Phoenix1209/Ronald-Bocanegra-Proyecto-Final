dd09a5c6a42cc6f79a23a45d651ae6ebcefd9c69
Phoenix <charlesch129@protonmail.com>
2025-12-09 22:31:12 -0500

    Creado menu con js

---
+++ assets/js/nav.js
@@ -0,0 +1,21 @@
+/* 
+    nav.js
+    ----------------------------------------
+    Este script carga dinámicamente el menú 
+    de navegación desde nav.html y lo inserta 
+    en el elemento con id "nav-placeholder".
+*/
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    // Solicitar archivo del menú
+    fetch("../../nav.html")
+        .then(response => response.text()) // Convertir respuesta a texto
+        .then(html => {
+
+            // Insertar el menú dentro del contenedor
+            document.getElementById("nav-placeholder").innerHTML = html;
+
+        })
+        .catch(error => console.error("Error cargando el menú:", error));
+});
